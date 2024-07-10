1683480c6837201958e1c703b9ab24c5d7325ab9
Ravi Kumar <ravikumar46931@gmail.com>
2024-07-10 12:47:07 +0530

    version v0.0.2

---
+++ README.md
@@ -0,0 +1,16 @@
+# My Project
+
+<!-- ![PyPI](https://img.shields.io/pypi/v/my_project)
+![License](https://img.shields.io/github/license/yourusername/my_project)
+![Build Status](https://github.com/yourusername/my_project/actions/workflows/python-publish.yml/badge.svg) -->
+
+## Overview
+
+My Project is a Python library that provides a simple function to return a greeting message. This is a basic example to demonstrate the structure of a Python package and how to publish it on PyPI using GitHub Actions.
+
+## Installation
+
+You can install the library using pip:
+
+```sh
+pip install my_project
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from typing import List
+
+with open("README.md", "r", encoding="utf-8") as f:
+    next(f)  # Ignoring first line
+    long_description = f.read()
+
+
+## Required files
+__version__ = "0.0.15"
+REPO_NAME = "dstemplatest-pypi"
+PKG_NAME = "dstemplatest"  # package will be visible with this name in PyPI
+AUTHOR_USER_NAME = "ravi46931"  # GitHub user name
+AUTHOR_EMAIL = "ravikumar46931@gmail.com"
+
+setup(
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Python package for creating project structure.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    # Install requires may be added at here
+    install_requires=[
+        "pandas",
+        "numpy"  
+    ],
+)
